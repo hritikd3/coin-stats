e1a553d30aceea2337f0cd48adfae9291c4f1cc8
hritikd3 <hritikdangi3@gmail.com>
2022-09-23 13:44:29 +0530

    basic structure files created

---
+++ src/pages/coin-details/CoinDetails.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CoinDetails.css";
+
+const CoinDetails = () => {
+  return <div>CoinDetails</div>;
+};
+
+export default CoinDetails;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./home/Home";
+import CoinDetails from "./coin-details/CoinDetails";
+
+
+export { Home, CoinDetails };
