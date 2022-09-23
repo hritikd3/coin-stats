6a282b9690f809f11d2760875b49253119d2560a
hritikd3 <hritikdangi3@gmail.com>
2022-09-23 14:40:26 +0530

    added typography from materialui

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -6,8 +6,9 @@ import { Typography } from "@material-ui/core";
 function Banner() {
   return (
     <div className="banner">
-      <Typography varient="h1" align="center">
-        Crypto Tracker
+      <Typography variant="h2"
+      align="center" >
+        h1. Heading
       </Typography>
       <Carousel />
     </div>
