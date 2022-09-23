123cf8e96f6b65ef9c146c0534493eff06900eaf
hritikd3 <hritikdangi3@gmail.com>
2022-09-23 14:50:23 +0530

    updated banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Carousel from "../carousel/Carousel";
 import "./Banner.css";
-import { Typography } from "@material-ui/core";
+import { Typography, makeStyles } from "@material-ui/core";
+
+
+const useStyles = makeStyles(theme=>{
+    return {
+        banner :{
+            backgroundImage: 'url(./banner2.jpg)'
+        }
+    }
+})
 
 function Banner() {
+    const classes=useStyles();
   return (
-    <div className="banner">
+    <div className={classes.banner}>
       <Typography variant="h2"
       align="center" >
         h1. Heading
