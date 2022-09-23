05cb1dd76d8d60d6196264a1f5ec206f67747826
hritikd3 <hritikdangi3@gmail.com>
2022-09-23 15:17:01 +0530

    refcatored

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Carousel from "../carousel/Carousel";
-import "./Banner.css";
-import { Typography, makeStyles } from "@material-ui/core";
+import "./banner-styles.js";
+import { Typography } from "@material-ui/core";
+import useStyles from "./banner-styles.js";
 
-
-const useStyles = makeStyles(theme=>{
-    return {
-        banner :{
-            backgroundImage: 'url(./banner2.jpg)'
-        }
-    }
-})
 
 function Banner() {
     const classes=useStyles();
   return (
     <div className={classes.banner}>
       <Typography variant="h2"
-      align="center" >
-        h1. Heading
+      align="center"
+      color='primary'
+      >
+       Heading
+      </Typography>
+      <Typography variant="subtitle"
+      align="center"
+      color='secondary'
+      >
+      Get all information regardin your favourite ceypto vcoins
       </Typography>
       <Carousel />
     </div>
